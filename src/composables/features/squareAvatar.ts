497648924e771ad069e1ecca404ecca969c4fa12
eki1563 <ekikaizeng@outlook.com>
2023-05-28 20:44:33 +0800

    feature(头像): 圆角优化

--- src/composables/features/squareAvatar.ts
+++ src/composables/features/squareAvatar.ts
@@ -4,7 +4,8 @@ import { getCSSOM, removeRules } from '@/utils'
 
 let CSSOM: CSSStyleSheet
 
-const radiusSelector: string = '.kook-avatar .kook-avatar-picture-animate, .kook-avatar .kook-avatar-picture-static, .user-info-card .user-avatar-panel .user-avatar-img, .conversation-list .user-avatar-inner, .friend-user-avatar-inner'
+const radiusSelector: string = '.kook-avatar .kook-avatar-picture-animate, .kook-avatar .kook-avatar-picture-static, .user-info-card .user-avatar-panel .user-avatar-img, .conversation-list .user-avatar-inner, .friend-user-avatar-inner, .kook-avatar .kook-avatar-mask'
+const backgroundSelector: string = '.text-message-item .message-user-head, .kook-avatar-picture-static, .kook-avatar-picture-animate'
 
 export function init() {
   if (!CSSOM) {
@@ -28,12 +29,15 @@ export function useSetAvatarRadius(radius: string, saveConfig = true) {
   init()
   const appendCSSContent = `${ radiusSelector } {border-radius: ${ radius.trim() } !important;}`
   removeRules(radiusSelector)
+  removeRules(backgroundSelector)
   CSSOM.insertRule(appendCSSContent)
+  CSSOM.insertRule(`${ backgroundSelector } {background-color: transparent !important;}`)
   saveConfig && storageHelper.setKey(STORAGE_KEYS.KOOK_HELPER_LITE_AVATAR_RADIUS, radius)
 }
 
 export function useResetAvatarRadius(saveConfig = true) {
   init()
   removeRules(radiusSelector)
+  removeRules(backgroundSelector)
   saveConfig && storageHelper.removeKey(STORAGE_KEYS.KOOK_HELPER_LITE_AVATAR_RADIUS)
 }
